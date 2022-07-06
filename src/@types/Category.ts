0d0e7a475fe41249caaa82c05972fa8cbfb2d358
Mariia <ma.gaiduchenko@gmail.com>
2022-07-06 21:22:17 +0300

    added movie interface and enum category

---
+++ src/@types/Category.ts
@@ -0,0 +1,7 @@
+enum Category {
+    Popular = 'popular',
+    TopRated = 'top_rated',
+    Upcoming = 'upcoming',
+}
+
+export { Category };
---
+++ src/@types/MovieMapped.ts
@@ -0,0 +1,10 @@
+interface MovieMapped{
+    backdrop_path: string;
+    id: number;
+    overview: string;
+    poster_path: string;
+    title: string;
+    release_date: number;
+}
+
+export { MovieMapped }
