86a3fd1ba7d4f63a86e33743ae56bb0fcbeafe05
Mariia <ma.gaiduchenko@gmail.com>
2022-07-08 17:52:56 +0300

    render app

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
+import loadMore from "./elements/helpers/loadMore";
+import searchMovie from "./elements/helpers/searchMovie";
+import { categoryChange } from "./elements/helpers/selectCategory";
+import { fetchMovies } from "./fetch/fetchMovies";
+import renderMovies from "./render/renderMovies";
+
 export async function render(): Promise<void> {
     // TODO render your app here
+    const movies = await fetchMovies();
+    renderMovies(movies);
+
+    const loadMoreButton: HTMLButtonElement = document.getElementById(
+        'load-more'
+    ) as HTMLButtonElement;
+    loadMoreButton.onclick = loadMore;
+
+    categoryChange();
+
+    const form: HTMLFormElement = document.getElementById(
+        'search-form'
+    ) as HTMLFormElement;
+    form.onsubmit = searchMovie;
+
+    // searching as typing
+    const input: HTMLInputElement = document.getElementById(
+        'search'
+    ) as HTMLInputElement;
+    let timer: NodeJS.Timeout;
+    input.addEventListener('input', (e: Event) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            searchMovie(e);
+        }, 500);
+    });
+   
 }
